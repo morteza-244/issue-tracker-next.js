5f6e5f4f51e4b9b99fb412f311f12b01e9b2bded
morteza <mori2002sa@gmail.com>
2024-03-18 22:32:05 +0330

    Update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -31,7 +31,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = async (data: TIssueFormData) => {
     try {
       setIsSubmitting(true);
-      await axios.post("/api/issues", data);
+      if (issue) await axios.patch(`/api/issues/${issue.id}`, data);
+      else await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
       setIsSubmitting(false);
