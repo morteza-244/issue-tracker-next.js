206513f526549a20b0f7495c4fd8846a421d4fbc
morteza <mori2002sa@gmail.com>
2024-03-19 20:05:25 +0330

    Change the layout of the navbar/

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 import classNames from "classnames";
 import { Bug } from "lucide-react";
 import { useSession } from "next-auth/react";
@@ -14,34 +14,40 @@ const NavBar = () => {
     { label: "Issues", href: "/issues/list" },
   ];
   return (
-    <nav className="flex items-center gap-5 bg-slate-50 shadow-lg p-5">
-      <Link href={"/"}>
-        <Bug size={32} />
-      </Link>
-      <ul className="gap-5 flex">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              href={link.href}
-              className={classNames({
-                "text-zinc-900": link.href === pathname,
-                "text-zinc-500": link.href !== pathname,
-                "hover:text-zinc-800 transition-colors font-medium": true,
-              })}
-            >
-              {link.label}
+    <nav className="gap-5 bg-slate-50 shadow-lg p-5">
+      <Container>
+        <Flex justify={"between"}>
+          <Flex align={"center"} gap={"4"}>
+            <Link href={"/"}>
+              <Bug size={32} />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" && (
-          <Link href="/api/auth/signout">Logout</Link>
-        )}
-        {status === "unauthenticated" && (
-          <Link href="/api/auth/signin">Login</Link>
-        )}
-      </Box>
+            <ul className="gap-5 flex">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    href={link.href}
+                    className={classNames({
+                      "text-zinc-900": link.href === pathname,
+                      "text-zinc-500": link.href !== pathname,
+                      "hover:text-zinc-800 transition-colors font-medium": true,
+                    })}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Flex align={'center'}>
+            {status === "authenticated" && (
+              <Link href="/api/auth/signout" className="font-medium">Logout</Link>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin" className="font-medium">Login</Link>
+            )}
+          </Flex>
+        </Flex>
+      </Container>
     </nav>
   );
 };
