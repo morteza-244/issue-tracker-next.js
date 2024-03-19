ad52e68f3265326299b70d9f33a39d3fdc295fad
morteza <mori2002sa@gmail.com>
2024-03-19 23:54:52 +0330

    Validate login form by zod

--- app/auth/signIn/page.tsx
+++ app/auth/signIn/page.tsx
@@ -3,25 +3,34 @@ import { Box, Button, Text, TextField } from "@radix-ui/themes";
 import { LogIn } from "lucide-react";
 import { useForm } from "react-hook-form";
 import FormHeading from "../_components/FormHeading";
+import { signInUserSchema, TSignInFormData } from "@/app/validations";
+import { ErrorMessage } from "@/app/components";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface SignInFormData {
-  email: string;
-  password: string;
-}
 const SignInPage = () => {
-  const { register, handleSubmit } = useForm<SignInFormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TSignInFormData>({
+    resolver: zodResolver(signInUserSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-  const onSubmit = (data: SignInFormData) => {
+  const onSubmit = (data: TSignInFormData) => {
     console.log(data);
   };
   return (
-    <div className="max-w-96 border p-4 bg-slate-100 mx-auto rounded-xl mt-10 space-y-4">
+    <div className="max-w-96 p-4 bg-slate-100 mx-auto rounded-xl mt-10 space-y-4">
       <FormHeading
         title="Login to your account"
         description="Welcome back! please enter your details"
       />
       <form className="space-y-7" onSubmit={handleSubmit(onSubmit)}>
-        <Box>
+        <Box className="space-y-2">
           <Text as="p" mb={"2"}>
             Email
           </Text>
@@ -31,14 +40,16 @@ const SignInPage = () => {
               {...register("email")}
             />
           </TextField.Root>
+          <ErrorMessage>{errors.email?.message}</ErrorMessage>
         </Box>
-        <Box>
+        <Box className="space-y-2">
           <Text as="p" mb={"2"}>
             Password
           </Text>
           <TextField.Root>
             <TextField.Input placeholder="Password" {...register("password")} />
           </TextField.Root>
+          <ErrorMessage>{errors.password?.message}</ErrorMessage>
         </Box>
         <Box className="text-center mt-5">
           <Button>
